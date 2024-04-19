cf0acf62722625bd74535537ad7c34478c234fd6
Ahsan ullah <ahsanullah0216@gmail.com>
2024-04-19 22:12:17 +0600

    user model/migration/seed

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+Route::get('/test', function (Request $request) {
+    // return $request->user();
+    return response()->json($request);
+});
