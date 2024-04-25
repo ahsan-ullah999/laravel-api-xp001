6703b5e06fb8b147e52763837851d45f7cb50846
Ahsan ullah <ahsanullah0216@gmail.com>
2024-04-25 20:59:00 +0600

    Define the relationship in the user and post model

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function post(){
+        return $this->hasOne(Post::class);
+    }
+
+
+
+
 }
+
+
+
