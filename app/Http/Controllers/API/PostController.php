75e20ffb944129feeba26f7efd1c4cd26c655d07
Ahsan ullah <ahsanullah0216@gmail.com>
2024-05-03 00:02:48 +0600

    Edit the post create methood

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace App\Http\Controllers\API;
-
+use App\Models\Post;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use Illuminate\Http\Request;
-use App\Models\Post;
+
 
 class PostController extends Controller
 {
@@ -21,6 +21,8 @@ class PostController extends Controller
             $posts = Post::with('user')->get();
             return PostResource::collection($posts);
 
+            return Post::all();
+
     }
 
     /**
@@ -28,26 +30,7 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        Post::create([
-            'title'=> $request->title,
-             'tag'=> $request->tag,
-             'description'=> $request->description,
-             'author'=> $request->author,
-
-        ]);
-        return response()->json( 201);
-        // $posts= new Post();
-        // $posts->title = $request->input('title');
-        // $posts->tag = $request->input('tag');
-        // $posts->description = $request->input('description');
-        // $posts->author = $request->input('author');
-        // $posts->save();
-
-
-        // return response()->json($posts, 201);
-
-
-
+        return Post::create($request->all());
 
     }
 
@@ -62,76 +45,8 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function post_update(Request $request, Post $id)
+    public function update(Request $request, $id)
     {
-
-        //  $posts= Post::find($id)-> $request->all();
-        //  $posts->title= $request->title;
-        //  $posts->tag= $request->tag;
-        //  $posts->description= $request->description;
-        //  $posts->author= $request->author;
-
-
-
-        //      if($request->id){
-        //        return response()->json( 201);
-        //      }else{
-        //        return response()->json("error", 404);
-        //      }
-
-
-
-
-
-
-
-        //    $posts= $request->all();
-        //    $model=Post::find($id);
-        //    if($model){
-        //       $model->update($posts);
-        //        return response()->json($model,200);
-
-        //    }else{
-        //        return response()->json(['error'=>'resource not found'],404);
-
-
-        //    }
-
-
-
-
-
-
-
-
-        // Post::find($id)->update([
-        //    'title'=> $request->title,
-        //    'tag'=> $request->tag,
-        //    'description'=> $request->description,
-        //    'author'=> $request->author,
-
-        //    ]);
-        //    return response()->json( 201);
-
-
-
-
-
-             $posts= Post::find($request->$id);
-             $posts->title= $request->title;
-             $posts->tag= $request->tag;
-             $posts->description= $request->description;
-             $posts->author= $request->author;
-             $result= $posts->save();
-
-             if($result){
-               return response()->json( 201);
-             }else{
-              return response()->json(['error'=>'resource not found'],404);
-             }
-
-
-
 
 
     }
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
-use App\Models\Post;
 use App\Http\Controllers\API\PostController;
-use App\Http\Controllers\API\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,20 +15,16 @@ use App\Http\Controllers\API\UserController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/posts',[PostController::class,'index']);
+
+Route::post('/posts',[PostController::class,'store']);
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-// function(){
-//     $posts = Post::get();
-//     return response()->json($posts);
 
-// }
-
-
-Route::get('/posts',[PostController::class,'index']);
-Route::post('/posts',[PostController::class,'store'])->name('post.store');
-Route::put('/posts/update/{id}',[PostController::class,'update'])->name('post_update');
 
 
 
