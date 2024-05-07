67035d21c86e0204da57ef4c05e4c7d1b85b1cc3
Ahsan ullah <ahsanullah0216@gmail.com>
2024-05-08 00:15:30 +0600

    update migration and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-          \App\Models\User::factory(10)->create();
-          \App\Models\Post::factory(10)->create();
+          \App\Models\User::factory(15)->create();
+          \App\Models\Post::factory(15)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
